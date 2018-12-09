bbce8fe403a5e80f6abfb4e7c6c96684fa6362f7
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-09 13:57:20 -0800

    Attempt to initialize gpiozero MotionSensor on Vision Bonnet's PIN_A

---
+++ motion.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import ptvsd
+ptvsd.enable_attach()
+ptvsd.wait_for_attach()  # blocks execution until debugger is attached
+
+import os
+import sys
+
+sys.path.append(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'aiyprojects-raspbian'))
+
+import gpiozero
+import pins
+
+motion_sensor = gpiozero.MotionSensor(pins.PIN_A, pull_up=True)
+
+if __name__ == '__main__':
+    print('hello')
