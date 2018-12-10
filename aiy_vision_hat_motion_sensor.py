9469f2441171e9c4c2cb37cd1f12f4a4b322bc65
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-09 17:38:23 -0800

    AIYVisionHatMotionSensor

---
+++ aiy_vision_hat_motion_sensor.py
@@ -0,0 +1,25 @@
+from gpiozero import MotionSensor
+
+class AIYVisionHatMotionSensor(MotionSensor):
+    """
+    Inverts MotionSensor from gpiozero and hard-codes pull_up to True
+    """
+    def __init__(
+            self, pin=None, queue_len=1, sample_rate=10, threshold=0.5,
+            partial=False, pin_factory=None):
+        super(AIYVisionHatMotionSensor, self).__init__(
+            pin=pin, queue_len=queue_len, sample_rate=sample_rate, threshold=threshold,
+            partial=partial, pin_factory=pin_factory,
+            pull_up=True # AIY doesn't support False
+        )
+
+
+def _negate(func):
+    return lambda *args, **kwds: not func(*args, **kwds)
+
+
+AIYVisionHatMotionSensor.motion_detected = _negate(super.motion_detected)
+AIYVisionHatMotionSensor.when_motion = MotionSensor.when_no_motion
+AIYVisionHatMotionSensor.when_no_motion = MotionSensor.when_motion
+AIYVisionHatMotionSensor.wait_for_motion = MotionSensor.wait_for_no_motion
+AIYVisionHatMotionSensor.wait_for_no_motion = MotionSensor.wait_for_motion
--- motion.py
+++ motion.py
@@ -7,25 +7,15 @@ sys.path.append(os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
     'aiyprojects-raspbian/src/aiy'))
 
-import gpiozero
 import pins
 import signal
 import datetime
-import functools
+from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
 
 def cur_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
-class InvertedMotionSensor(gpiozero.MotionSensor):
-    pass
-
-InvertedMotionSensor.motion_detected    = lambda *args, **kwds: not super.motion_detected(*args, **kwds)
-InvertedMotionSensor.when_motion        = lambda *args, **kwds: super.when_no_motion(*args, **kwds)
-InvertedMotionSensor.when_no_motion     = lambda *args, **kwds: super.when_motion(*args, **kwds)
-InvertedMotionSensor.wait_for_motion    = lambda *args, **kwds: super.wait_for_no_motion(*args, **kwds)
-InvertedMotionSensor.wait_for_no_motion = lambda *args, **kwds: super.wait_for_motion(*args, **kwds)
-
-motion_sensor = InvertedMotionSensor(pins.PIN_A, pull_up=True)
+motion_sensor = AIYVisionHatMotionSensor(pins.PIN_A)
 motion_sensor.when_motion = lambda: print('{}: motion'.format(cur_time()))
 motion_sensor.when_no_motion = lambda: print('{}: no_motion'.format(cur_time()))
 
