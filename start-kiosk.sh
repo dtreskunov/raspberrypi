0ff04ac97794208507441071ede1e03d749a8ac7
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-09 23:38:40 -0800

    start-kiosk.sh

---
+++ start-kiosk.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# replace /home/pi/.config/lxsession/LXDE-pi/autostart with:
+#
+# @/home/pi/raspberrypi/start-kiosk.sh
+
+set -uo pipefail
+
+SERVER=http://localhost
+
+kiosk_mode() {
+    xset -dpms # disable DPMS (Energy Star) features.
+    xset s off # disable screen saver
+    xset s noblank # don’t blank the video device
+    unclutter # hide mouse pointer
+}
+
+check_server() {
+    curl --silent --fail --head -o /dev/null "$SERVER"
+}
+
+wait_for_server() {
+    while true; do
+        check_server
+        if [ $? ]; then
+            break
+        fi
+        echo 'waiting for server to come up...'
+        sleep 5
+    done
+}
+
+kiosk_mode
+wait_for_server
+midori -e Fullscreen -a "$SERVER"
