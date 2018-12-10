8ff69b4ef7a0e8e50900aa172efe6594cdf49d5b
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-09 17:55:45 -0800

    play_tetris_theme

--- motion.py
+++ motion.py
@@ -8,6 +8,7 @@ sys.path.append(os.path.join(
     'aiyprojects-raspbian/src/aiy'))
 
 import pins
+from aiy.toneplayer import TonePlayer
 import signal
 import datetime
 from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
@@ -15,9 +16,40 @@ from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
 def cur_time():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
+def play_tetris_theme(bpm=120):
+    player = TonePlayer(bpm=bpm)
+    player.play(
+        'E5q',
+        'Be',
+        'C5e',
+        'D5e',
+        'E5s',
+        'D5s',
+        'C5s',
+        'Be',
+        'Bs',
+        'Aq',
+        'Ae',
+        'C5e',
+        'E5q',
+        'D5e',
+        'C5e',
+        'Bq',
+        'Be',
+        'C5e',
+        'D5q',
+        'E5q',
+        'C5q',
+        'Aq',
+        'Aq'
+    )
+
+
 motion_sensor = AIYVisionHatMotionSensor(pins.PIN_A)
 motion_sensor.when_motion = lambda: print('{}: motion'.format(cur_time()))
 motion_sensor.when_no_motion = lambda: print('{}: no_motion'.format(cur_time()))
+
+motion_sensor.when_motion = play_tetris_theme
 
 if __name__ == '__main__':
     if '--debug' in sys.argv:
