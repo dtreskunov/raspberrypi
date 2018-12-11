71f11b04c204c2e2f0a499ef681d084f925d0ef9
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-11 00:17:08 -0800

    maybe like this?

--- sensors-server/main.py
+++ sensors-server/main.py
@@ -4,39 +4,23 @@ import asyncio
 import logging
 import sys
 
-from async_generator import async_generator, yield_
-
 import setup_aiy_path
 from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
 from websocket_broadcaster import WebsocketBroadcaster
 
 
-@async_generator
-async def motion_sensor_events():
-    motion_detected = asyncio.Event()
-
-    async def driver_loop():
-        import aiy.pins
-        sensor = AIYVisionHatMotionSensor(aiy.pins.PIN_A)
-        sensor.when_motion = lambda: motion_detected.set()
-        while True:
-            await asyncio.sleep(100)
-
-    asyncio.ensure_future(driver_loop())
-
+async def motion_sensor(callback):
+    import aiy.pins
+    sensor = AIYVisionHatMotionSensor(aiy.pins.PIN_A)
+    sensor.when_motion = lambda: callback('motion_detected')
     while True:
-        await motion_detected.wait()
-        motion_detected.clear()
-        await yield_('motion_detected')
-
+        await asyncio.sleep(10)
 
 async def main():
     async with WebsocketBroadcaster() as server:
-        async def send_motion_sensor():
-            async for event in motion_sensor_events():
-                server.broadcast({'sensor': 'motion_sensor', 'event': event})
-
-        await asyncio.wait([send_motion_sensor()])
+        await asyncio.wait([
+            motion_sensor(lambda event: server.broadcast({'sensor': 'motion_sensor', 'event': event}))
+        ])
 
 if __name__ == '__main__':
     if '--debug' in sys.argv:
