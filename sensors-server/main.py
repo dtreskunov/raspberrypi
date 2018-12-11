904caac3b38170012ffd0e99b73c673da8e3655f
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-11 13:11:40 -0800

    replace websockets with mqtt
    apt-get install mosquitto mosquitto-clients

--- sensors-server/main.py
+++ sensors-server/main.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
 
+import argparse
 import asyncio
+import concurrent.futures
+import json
 import logging
 import sys
+from functools import partial
+
+import paho.mqtt.client as mqtt
 
 import setup_aiy_path
 from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
-from websocket_broadcaster import WebsocketBroadcaster
+
+logger = logging.getLogger(__name__)
 
 
-async def motion_sensor(callback):
+async def motion_sensor_task(callback):
+    logger.info('starting motion_sensor_task')
     import aiy.pins
     sensor = AIYVisionHatMotionSensor(aiy.pins.PIN_A)
     sensor.when_motion = lambda: callback('motion_detected')
     while True:
         await asyncio.sleep(1)
 
-async def temperature_humidity_sensor(callback):
+
+async def temperature_humidity_sensor_task(callback):
+    logger.info('starting temperature_humidity_sensor_task')
     import aiy.pins
     import Adafruit_DHT
     while True:
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-        humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, aiy.pins.PIN_B.gpio_spec.pin)
+        humidity, temperature = Adafruit_DHT.read_retry(
+            Adafruit_DHT.DHT11, aiy.pins.PIN_B.gpio_spec.pin)
         if humidity is not None:
             callback({'humidity': humidity})
         if temperature is not None:
@@ -30,22 +41,59 @@ async def temperature_humidity_sensor(callback):
         await asyncio.sleep(10)
 
 
-async def main():
-    async with WebsocketBroadcaster() as server:
-        tasks = [
-            motion_sensor(lambda event: server.broadcast({'sensor': 'motion_sensor', 'event': event})),
-            temperature_humidity_sensor(lambda event: server.broadcast({'sensor': 'temperature_humidity_sensor', 'event': event}))
-        ]
-        for x in asyncio.as_completed(tasks):
-            try:
-                await x
-            except Exception as e:
-                print('task finished with exception: {}'.format(e))
-        print('all tasks finished')
+def mqtt_client(host, port):
+    client = mqtt.Client()
+    client.reconnect_delay_set()
+
+    def on_connect(client, userdata, flags, rc):
+        # add subscriptions here
+        logger.info('Connected to MQTT broker %s:%s, flags=%s, rc=%s',
+                    host, port, flags, rc)
+
+    def on_disconnect(client, userdata, rc):
+        logger.info('Disconnected from MQTT broker %s:%s, rc=%s',
+                    host, port, rc)
+
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.connect_async(host, port)
+    return client
+
+
+async def main(host, port):
+    client = mqtt_client(host, port)
+    client.loop_start()
+
+    def publish(topic, data):
+        msg = json.dumps(data)
+        logger.info('publishing to %s: %s', topic, msg)
+        client.publish(topic, msg)
+
+    tasks = [
+        motion_sensor_task(partial(publish, 'sensor/motion')),
+        temperature_humidity_sensor_task(
+            partial(publish, 'sensor/temperature_humidity')),
+    ]
+    for x in asyncio.as_completed(tasks):
+        try:
+            await x
+            logger.warning('sensor task completed unexpectedly')
+        except Exception as e:
+            logger.error('sensor task died with exception: %s', e)
+    logger.info('all sensor tasks completed')
 
 
 if __name__ == '__main__':
-    if '--debug' in sys.argv:
+    parser = argparse.ArgumentParser(
+        description='Broadcasts Raspberry Pi sensor readings to MQTT broker')
+    parser.add_argument(
+        '-d', '--debug', help='enable remote debugger compatible with VS Code', action='store_true')
+    parser.add_argument(
+        '--host', help='MQTT broker host', default='localhost')
+    parser.add_argument(
+        '--port', help='MQTT broker port', default=1883, type=int)
+    args = parser.parse_args()
+    if args.debug:
         import ptvsd
         address = ('0.0.0.0', 5678)
         ptvsd.enable_attach(address=address)
@@ -54,4 +102,4 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    loop.run_until_complete(main(args.host, args.port))
