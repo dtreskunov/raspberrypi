0565de7c9c993cae1dfafafac73ff14b0b681455
Denis Treskunov <denis.treskunov@gmail.com>
2018-12-12 15:19:31 -0800

    Add face recognition
    Rename MQTT topic 'sensor/face_detection' to 'sensor/face_recognition'.
    
    Messages published to the topic will have an additional field, 'face_descriptor',
    which is a 128-d vector describing face identity.
    
    Using an algorithm like ANN (Approximate Nearest Neighbor) or LSH
    (Locality-Sensitive Hashing) is left as an exercise for the reader :)
    
    https://stackoverflow.com/questions/5751114/nearest-neighbors-in-high-dimensional-data

--- sensors-server/main.py
+++ sensors-server/main.py
@@ -3,19 +3,26 @@
 import argparse
 import asyncio
 import base64
+import bz2
 import concurrent.futures
 import contextlib
+import glob
 import io
 import json
 import logging
+import os.path
 import sys
+import time
+import urllib.request
 from functools import partial
 
+import numpy
 import paho.mqtt.client as mqtt
 import PIL.Image
 
 import Adafruit_DHT
 import aiy.pins
+import dlib
 import picamera
 import picamera.array
 from aiy.leds import Color, Leds, Pattern, PrivacyLed
@@ -28,15 +35,26 @@ from aiy_vision_hat_motion_sensor import AIYVisionHatMotionSensor
 logger = logging.getLogger(__name__)
 
 
-async def face_detection_task(callback):
-    logger.info('starting face_detection_task')
+@contextlib.contextmanager
+def stopwatch(message):
+    try:
+        begin = time.monotonic()
+        yield
+    finally:
+        end = time.monotonic()
+        logger.info('%s: done in %.3fs', message, end - begin)
+
+
+async def face_recognition_task(callback):
+    logger.info('starting face_recognition_task')
 
     def capture_image(camera):
-        stream = io.BytesIO()
-        camera.capture(stream, format='jpeg')
-        # "Rewind" the stream to the beginning so we can read its content
-        stream.seek(0)
-        return PIL.Image.open(stream)
+        with stopwatch('capture_image'):
+            stream = io.BytesIO()
+            camera.capture(stream, format='jpeg')
+            # "Rewind" the stream to the beginning so we can read its content
+            stream.seek(0)
+            return PIL.Image.open(stream)
 
     def image_to_data_uri(image):
         stream = io.BytesIO()
@@ -52,37 +70,84 @@ async def face_detection_task(callback):
                 res_w, res_h, img_w, img_h))
         return img_w / res_w
 
-    def process_inference_result(inference_result, camera):
+    def get_dlib_data():
+        '''
+        Returns a tuple of filenames, downloading from dlib.net and extracting if necessary:
+        1. shape predictor data for dlib.shape_predictor (shape_predictor_5_face_landmarks.dat)
+        2. face recognition data for dlib.face_recognition_model_v1 ()
+        '''
+        def get(url, dest):
+            'returns normalized destination path'
+            dest = os.path.realpath(os.path.expanduser(dest))
+            if not os.path.isfile(dest):
+                with stopwatch('downloading from %s and extracting into %s' % (url, dest)):
+                    resp = urllib.request.urlopen(url)
+                    bz2_file = bz2.BZ2File(io.BytesIO(resp.read()))
+                    with open(dest, 'wb') as dest_file:
+                        dest_file.write(bz2_file.read())
+            return dest
+
+        url_dests = (
+            ('http://dlib.net/files/shape_predictor_5_face_landmarks.dat.bz2',
+             '~/.shape_predictor_5_face_landmarks.dat'),
+            ('http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2',
+             '~/.dlib_face_recognition_resnet_model_v1.dat')
+        )
+        return tuple(get(url, dest) for url, dest in url_dests)
+
+    def get_face_descriptor(image, shape_predictor, face_recognition_model):
+        '''
+        Compute the 128D vector that describes the face in image.
+        In general, if two face descriptor vectors have a Euclidean
+        distance between them less than 0.6 then they are from the same
+        person, otherwise they are from different people.
+        '''
+        with stopwatch('get_face_descriptor'):
+            image_arr = numpy.array(image)
+            face_landmarks = shape_predictor(
+                image_arr,
+                dlib.rectangle(left=0, top=0, right=image.width, bottom=image.height))
+            face_descriptor = face_recognition_model.compute_face_descriptor(
+                image_arr, face_landmarks)
+            return face_descriptor
+
+    def process_inference_result(inference_result, camera, shape_predictor, face_recognition_model):
         faces = face_detection.get_faces(inference_result)
         if not faces:
             return
 
-        logging.debug(
-            'detected %s faces, taking snapshot for recognition', len(faces))
-        # inference runs on the vision bonnet, which grabs images from the camera directly
-        # we need to capture the image separately on the Raspberry
-        image = capture_image(camera)
-        scale = get_scale(inference_result, image)
+        with stopwatch('process_inference_result for {} face(s)'.format(len(faces))):
+            # inference runs on the vision bonnet, which grabs images from the camera directly
+            # we need to capture the image separately on the Raspberry
+            image = capture_image(camera)
+            scale = get_scale(inference_result, image)
 
-        def sensor_data_iter():
-            for face in faces:
-                # translate inference result into img coordinates and slice out just the face
-                f_x, f_y, f_w, f_h = face.bounding_box
-                face_image = image.crop((
-                    max(0, int(scale * f_x)),  # left
-                    max(0, int(scale * f_y)),  # upper
-                    min(image.width, int(scale * (f_x + f_w))),  # right
-                    min(image.height, int(scale * (f_y + f_h))),  # lower
-                ))
-                # TODO: face recognition :)
-                yield {
-                    'face_score': face.face_score,
-                    'joy_score': face.joy_score,
-                    'image_uri': image_to_data_uri(
-                        face_image.resize(size=(100, 100), resample=PIL.Image.LANCZOS))
-                }
+            def sensor_data_iter():
+                for face in faces:
+                    # translate inference result into img coordinates and slice out just the face
+                    f_x, f_y, f_w, f_h = face.bounding_box
+                    face_image = image.crop((
+                        max(0, int(scale * f_x)),  # left
+                        max(0, int(scale * f_y)),  # upper
+                        min(image.width, int(scale * (f_x + f_w))),  # right
+                        min(image.height, int(scale * (f_y + f_h))),  # lower
+                    ))
+                    face_descriptor = get_face_descriptor(
+                        face_image, shape_predictor, face_recognition_model)
+                    yield {
+                        'face_score': face.face_score,
+                        'face_descriptor': face_descriptor,
+                        'joy_score': face.joy_score,
+                        'image_uri': image_to_data_uri(
+                            face_image.resize(size=(100, 100), resample=PIL.Image.LANCZOS)),
+                    }
 
-        callback(list(sensor_data_iter()))
+            callback(list(sensor_data_iter()))
+
+    shape_predictor_data, face_recognition_model_data = get_dlib_data()
+    shape_predictor = dlib.shape_predictor(shape_predictor_data)
+    face_recognition_model = dlib.face_recognition_model_v1(
+        face_recognition_model_data)
 
     with contextlib.ExitStack() as stack:
 
@@ -93,17 +158,18 @@ async def face_detection_task(callback):
             when trying to initialize CameraInference, we retry after resetting
             the InferenceEngine.
             '''
-            for _ in range(2):
-                try:
-                    return stack.enter_context(
-                        CameraInference(face_detection.model()))
-                except InferenceException as e:
-                    logger.info(
-                        'attempting to reset InferenceEngine due to: %s', e)
-                    with InferenceEngine() as engine:
-                        engine.reset()
-            else:
-                raise Exception('unable to start CameraInference')
+            with stopwatch('initialize_inference'):
+                for _ in range(2):
+                    try:
+                        return stack.enter_context(
+                            CameraInference(face_detection.model()))
+                    except InferenceException as e:
+                        logger.info(
+                            'attempting to reset InferenceEngine due to: %s', e)
+                        with InferenceEngine() as engine:
+                            engine.reset()
+                else:
+                    raise Exception('unable to start CameraInference')
 
         leds = stack.enter_context(Leds())
         stack.enter_context(PrivacyLed(leds))
@@ -117,7 +183,8 @@ async def face_detection_task(callback):
         inference = initialize_inference()
 
         for inference_result in inference.run():
-            process_inference_result(inference_result, camera)
+            process_inference_result(
+                inference_result, camera, shape_predictor, face_recognition_model)
 
 
 async def motion_sensor_task(callback):
@@ -143,6 +210,8 @@ async def temperature_humidity_sensor_task(callback):
 
 
 def mqtt_client(host, port):
+    logger.info(
+        'Connecting to MQTT broker %s:%s. Hint: you can monitor MQTT traffic by running: mosquitto_sub -h %s -p %s -t \'#\'', host, port, host, port)
     client = mqtt.Client()
     client.reconnect_delay_set()
 
@@ -150,8 +219,6 @@ def mqtt_client(host, port):
         # add subscriptions here
         logger.info('Connected to MQTT broker %s:%s, flags=%s, rc=%s',
                     host, port, flags, rc)
-        logger.info(
-            'Hint: you can monitor MQTT traffic by running `mosquitto_sub -h %s -p %s -t \'#\'', host, port)
 
     def on_disconnect(client, userdata, rc):
         logger.info('Disconnected from MQTT broker %s:%s, rc=%s',
@@ -173,7 +240,7 @@ async def main(host, port):
         client.publish(topic, msg)
 
     tasks = [
-        face_detection_task(partial(publish, 'sensor/face_detection')),
+        face_recognition_task(partial(publish, 'sensor/face_recognition')),
         motion_sensor_task(partial(publish, 'sensor/motion')),
         temperature_humidity_sensor_task(
             partial(publish, 'sensor/temperature_humidity')),
@@ -182,8 +249,8 @@ async def main(host, port):
         try:
             await x
             logger.warning('sensor task completed unexpectedly')
-        except Exception as e:
-            logger.error('sensor task died with exception: %s', e)
+        except Exception:
+            logger.exception('sensor task died')
     logger.info('all sensor tasks completed')
 
 
