215ecb5dfe3b51ce82c65bf3845dcf259259c11b
Denis Treskunov <denis.treskunov@gmail.com>
2019-01-05 23:18:47 -0800

    big update

--- sensors_server/face_recognition/web/main.py
+++ sensors_server/face_recognition/web/main.py
@@ -2,7 +2,7 @@ import inspect
 import logging
 
 from flask import Flask, jsonify, request
-from werkzeug.exceptions import BadRequest, NotFound
+from werkzeug.exceptions import BadRequest, Conflict, NotFound
 
 import face_recognition.database as db
 from face_recognition.image import MyImage
@@ -76,13 +76,26 @@ def post_entity(entity_type):
     return jsonify(_to_dict(entity))
 
 
-@app.route('/untagged_image_ids', methods=['GET'])
+@app.route('/ids/<entity_type>', methods=['GET'])
+@db.db_transaction
+def get_entity_ids(entity_type):
+    entity_class = _get_entity_class(entity_type)
+    return jsonify(
+        [str(id) for id in db.select(
+            entity.id
+            for entity in entity_class)])
+
+
+@app.route('/ids/Image/untagged', methods=['GET'])
 @db.db_transaction
 def get_untagged_image_ids():
     return jsonify(
         [str(id) for id in db.select(
-            image.id for image in db.Image if image in (db.select(
-                df.image for df in db.DetectedFace if df.person is None)))])
+            image.id
+            for image in db.Image
+            for face in db.DetectedFace
+            if face.image.id == image.id
+            and face.person is None)])
 
 
 @app.route('/link_face_to_person', methods=['POST'])
@@ -93,12 +106,35 @@ def link_face_to_person():
     face_id = payload.get('face_id', None)
     person_id = payload.get('person_id', None)
     if not face_id or not person_id:
-        raise BadRequest('Either "face_id" or "person_id" not found in request payload')
+        raise BadRequest(
+            'Either "face_id" or "person_id" not found in request payload')
     with db.db_transaction:
         face = db.DetectedFace[face_id]
         if not face:
             raise NotFound('Face with id "{}" not found'.format(face_id))
         face.person = person_id
+        return jsonify(_to_dict(face))
+
+
+@app.route('/unlink_face_from_person', methods=['POST'])
+def unlink_face_from_person():
+    payload = request.get_json()
+    if not payload:
+        raise BadRequest
+    face_id = payload.get('face_id', None)
+    person_id = payload.get('person_id', None)
+    if not face_id or not person_id:
+        raise BadRequest(
+            'Either "face_id" or "person_id" not found in request payload')
+    with db.db_transaction:
+        face = db.DetectedFace[face_id]
+        if not face:
+            raise NotFound('Face with id "{}" not found'.format(face_id))
+        if str(face.person.id) != person_id:
+            raise Conflict(
+                '{} is currently linked to {} - cannot unlink from {}'.format(
+                    face, face.person.id, person_id))
+        face.person = None
         return jsonify(_to_dict(face))
 
 
