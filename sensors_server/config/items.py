eb8ff8c48883bea8b3dfb9118a3c79bccfb4d5c9
Denis Treskunov <dtreskunov@tableau.com>
2019-01-31 20:22:47 -0800

    added static methods to Configurable
    `metadata()`: returns dict of all known configurables by name
    `serialized_values_by_name()`: returns dict of known configurables' serialized values
    `apply(serialized_values_by_name)`: applies given dict to known configurables

--- sensors_server/config/items.py
+++ sensors_server/config/items.py
@@ -1,4 +1,7 @@
 import abc
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ConfigItem(abc.ABC):
@@ -83,32 +86,51 @@ class MultipleChoiceConfigItem(ConfigItem):
     def extra_metadata(self):
         return {'choices': [self.serialize_value(choice) for choice in self._choices]}
 
+import weakref
 
 class Configurable(abc.ABC):
-    class ConfigItemsWrapper:
-        def __init__(self, config_items):
-            self._config_items = config_items
-            self._config_items_by_name = dict([
-                (config_item.name, config_item)
-                for config_item in config_items
+    
+    _name_to_wrapper = weakref.WeakValueDictionary()
+
+    class ItemsWrapper:
+        def __init__(self, items, name, description):
+            self._items = items
+            self._items_by_name = dict([
+                (item.name, item)
+                for item in items
             ])
+            self._name = name
+            self._description = description
+            Configurable._name_to_wrapper[self._name] = self
+    
+        @property
+        def name(self):
+            return self._name
         
+        @property
+        def description(self):
+            return self._description
+
         def __getattr__(self, name):
             try:
-                return self._config_items_by_name[name]
+                return self._items_by_name[name]
             except KeyError:
                 raise AttributeError('Config item "{}" not found'.format(name))
 
         def metadata(self):
-            return [{
-                'metadata': item.metadata(),
-                'extra_metadata': item.extra_metadata()
-            } for item in self._config_items]
+            return {
+                'name': self.name,
+                'description': self.description,
+                'items': [{
+                    'metadata': item.metadata(),
+                    'extra_metadata': item.extra_metadata()
+                } for item in self._items]
+            }
         
         def serialized_values_by_name(self):
             return dict([
                 (item.name, item.serialize_value(item.value))
-                for item in self._config_items
+                for item in self._items
             ])
         
         def validated_values_by_item(self, serialized_values_by_name):
@@ -129,18 +151,45 @@ class Configurable(abc.ABC):
             validated_values_by_item = self.validated_values_by_item(serialized_values_by_name)
             for item, value in validated_values_by_item.items():
                 if item.value != value:
-                    item.value = value
+                    item.set_value(value, validate=False)
                     updated = True
             return updated
 
 
-    def __init__(self, *config_items):
-        self._config_items_wrapper = Configurable.ConfigItemsWrapper(config_items)
+    def __init__(self, *config_items, config_name=None, config_description=None):
+        self._config_items_wrapper = Configurable.ItemsWrapper(
+            config_items,
+            config_name or self.__class__.__name__,
+            config_description)
     
     @property
     def config(self):
         return self._config_items_wrapper
         
+    @staticmethod
+    def metadata():
+        return dict([
+            (name, wrapper.metadata()) for name, wrapper in Configurable._name_to_wrapper.items()
+        ])
+    
+    @staticmethod
+    def apply(serialized_values_by_name):
+        applied = False
+        for name, serialized_values in serialized_values_by_name.items():
+            wrapper = Configurable._name_to_wrapper.get(name)
+            if wrapper is None:
+                logger.warning('Configurable `%s` not defined, unable to apply its config', name)
+                continue
+            applied = applied or wrapper.apply(serialized_values)
+        return applied
+        
+    @staticmethod
+    def serialized_values_by_name():
+        return dict([
+            (name, wrapper.serialized_values_by_name())
+            for name, wrapper in Configurable._name_to_wrapper.items()
+        ])
+    
 ###
 
 if __name__=='__main__':
@@ -169,4 +218,12 @@ if __name__=='__main__':
         
     t = TestClass()
     t.foo()
+    print(Configurable.metadata())
+    old_refinement_method_value = t.config.refinement_method.value
+    serialized = Configurable.serialized_values_by_name()
+    print(serialized)
+    t.config.refinement_method.value = 'HOG'
+    assert(old_refinement_method_value != t.config.refinement_method.value)
+    Configurable.apply(serialized)
+    assert(old_refinement_method_value == t.config.refinement_method.value)
 
